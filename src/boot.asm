a9e3faadddae8067dfe6507181baa71f69a97d0b
Dappster <lanebeals02@gmail.com>
2024-07-07 19:18:19 -0700

    Improvement to the bootloader

--- src/boot.asm
+++ src/boot.asm
@@ -1,10 +1,22 @@
-ORG 0x7C00
+ORG 0
 BITS 16
 
+jmp 0x7c0:start
+
 start:
+    cli ; Clear interrupts
+    mov ax, 0x7c0 ; We have to put the starting location into AX before moving it into the data segment or extra segment
+    mov ds, ax
+    mov es, ax
+
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+
+    sti ; Enables interrupts
     mov si, message
     call print
-    jmp $
+    jmp $   ; Infinite loop to halt execution
 
 print: 
     mov bx, 0
@@ -19,10 +31,10 @@ print:
     ret
 
 print_char:
-    mov ah, 0eh
-    int 0x10
+    mov ah, 0eh ; Move 0e to the high byte
+    int 0x10    ; Interrupt which tells the BIOS to print the character pointed by AL
     ret
 
-message: db 'Hello world!', 0
-times 510-($ - $$) db 0
-dw 0xAA55
+message: db 'Hello Lens_r!', 0
+times 510-($ - $$) db 0 ; Fill 510 bytes of memory with 0
+dw 0xAA55 ; Writes the boot signature (0x55AA since x86 is little endian)
